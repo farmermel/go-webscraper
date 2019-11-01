01a180f5b7967ac7ffd3dfda2d56a85d21da63c9
Melena Suliteanu <msuliteanu@pivotal.io>
2019-11-01 08:57:49 -0600

    Basic get accepts url from arguments

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
 func main() {
-	resp, err := http.Get("http://google.com")
+	body := handleGet()
+	println(body)
+}
+
+func handleGet() string {
+
+	url := os.Args[1:]
+
+	//check url[0] is a string/properly formatted
+	resp, err := http.Get(url[0])
 
 	if err != nil {
 		println(err)
@@ -11,12 +24,14 @@ func main() {
 
 	defer resp.Body.Close()
 
+	var bodyBytes []byte
 	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		bodyBytes, err = ioutil.ReadAll(resp.Body)
 		if err != nil {
-			log.Fatal(err)
+			println(err)
 		}
 
-		bodyString := 
 	}
+
+	return string(bodyBytes)
 }
